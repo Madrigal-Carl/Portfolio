303736989d02bd115bfacb062e1d3de8ad4238b1
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-05-25 21:44:35 +0800

    upate certificates

--- src/constants/certificates.js
+++ src/constants/certificates.js
@@ -1,22 +1,15 @@
+import base from "@/assets/certificates/base.png";
+import powerplatform from "@/assets/certificates/power-platform.png";
+
 export const CERTIFICATES = [
   {
-    title: "AWS Certified Solutions Architect",
-    year: 2024,
-    image: "http://static.photos/technology/640x360/11",
+    title: "Global Power Platform Bootcamp Philippines 2026",
+    year: 2026,
+    image: powerplatform,
   },
   {
-    title: "Google Cloud Professional Developer",
-    year: 2023,
-    image: "http://static.photos/technology/640x360/11",
-  },
-  {
-    title: "Meta Frontend Developer Certificate",
-    year: 2023,
-    image: "http://static.photos/technology/640x360/11",
-  },
-  {
-    title: "MongoDB Certified Developer",
-    year: 2022,
-    image: "http://static.photos/technology/640x360/11",
+    title: "Base Build MiMaRoPa - MARSU",
+    year: 2025,
+    image: base,
   },
 ];
