bd2dbf9b1683f972abaa64aa2d66eb858e2318a2
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-08-04 12:22:41 +0800

    agricentral project added

---
+++ src/assets/projects/agri/index.js
@@ -0,0 +1,45 @@
+import agriLogo from "./logo.png";
+import agri1 from "./agri1.png";
+import agri2 from "./agri2.png";
+import agri3 from "./agri3.png";
+import agri4 from "./agri4.png";
+import agri5 from "./agri5.png";
+import agri6 from "./agri6.png";
+import agri7 from "./agri7.png";
+import agri8 from "./agri8.png";
+import agri9 from "./agri9.png";
+import agri10 from "./agri10.png";
+import agri11 from "./agri11.png";
+import agri12 from "./agri12.png";
+import agri13 from "./agri13.png";
+import agri14 from "./agri14.png";
+import agri15 from "./agri15.png";
+import agri16 from "./agri16.png";
+import agri17 from "./agri17.png";
+import agri18 from "./agri18.png";
+import agri19 from "./agri19.png";
+import agri20 from "./agri20.png";
+
+export {
+    agriLogo,
+    agri1,
+    agri2,
+    agri3,
+    agri4,
+    agri5,
+    agri6,
+    agri7,
+    agri8,
+    agri9,
+    agri10,
+    agri11,
+    agri12,
+    agri13,
+    agri14,
+    agri15,
+    agri16,
+    agri17,
+    agri18,
+    agri19,
+    agri20,
+};
